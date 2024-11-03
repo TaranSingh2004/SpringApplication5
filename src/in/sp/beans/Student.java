28423d4bfb5d4f1e6798c94218c86306e81f50e2
TaranSingh2004 <taranduggal2004@gmail.com>
2024-11-03 11:28:38 +0530

    first commit

---
+++ src/in/sp/beans/Student.java
@@ -0,0 +1,38 @@
+package in.sp.beans;
+
+public class Student {
+    private int rollNo;
+    private String name;
+    private Address address;
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void display(){
+        System.out.println("rollno:" + rollNo);
+        System.out.println("name:" + name);
+        System.out.println("address:" + address);
+
+    }
+}
